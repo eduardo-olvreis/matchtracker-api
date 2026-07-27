69a9d368de25930bd066c1b65e16217dae7d206f
Eduardo <eduardo.olvreis@gmail.com>
2026-07-27 19:44:10 -0300

    feat: adiciona configuração inicial do banco de dados PostgreSQL

--- MatchTracker.Api/Program.cs
+++ MatchTracker.Api/Program.cs
@@ -1,7 +1,12 @@
+using MatchTracker.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
