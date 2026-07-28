4c318a88d2965c31f3c9b22c61a5366628183de3
Eduardo <eduardo.olvreis@gmail.com>
2026-07-28 15:56:54 -0300

    feat: adiciona Partidas na AppDbContext e InitialMigrations

--- MatchTracker.Api/Data/AppDbContext.cs
+++ MatchTracker.Api/Data/AppDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MatchTracker.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MatchTracker.Api.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Partida> Partidas { get; set; }
     }
 }
