0388963145017d66781626f77a857b52c795e7f4
Eduardo <eduardo.olvreis@gmail.com>
2026-07-28 15:49:26 -0300

    feat: adiciona PartidaCreateDto

---
+++ MatchTracker.Api/DTOs/Partidas/PartidaCreateDto.cs
@@ -0,0 +1,28 @@
+﻿using MatchTracker.Api.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatchTracker.Api.DTOs.Partidas
+{
+    public class PartidaCreateDto
+    {
+        [Required(ErrorMessage = "Campo 'Mapa' não pode ser nulo.")]
+        [EnumDataType(typeof(MapaCs2), ErrorMessage = "Mapa informado é inválido.")]
+        public MapaCs2? Mapa { get; set; }
+
+        [Range(0,100,ErrorMessage = "Campo 'Kills' deve estar entre o intervalo de 0 a 100.")]
+        public int Kills { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Campo 'Mortes' deve estar entre o intervalo de 0 a 100.")]
+        public int Mortes { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Campo 'Assistencias' deve estar entre o intervalo de 0 a 100.")]
+        public int Assistencias { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Resultado' não pode ser nulo.")]
+        [EnumDataType(typeof(ResultadoPartida), ErrorMessage = "Resultado informado é inválido.")]
+        public ResultadoPartida? Resultado { get; set; }
+
+        [Required(ErrorMessage = "Campo 'DataPartida' não pode ser nulo.")]
+        public DateTime DataPartida { get; set; }
+    }
+}
