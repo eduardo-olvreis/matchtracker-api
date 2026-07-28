b24f0c7b2e3f94e23c26d510cc769e23048aab22
Eduardo <eduardo.olvreis@gmail.com>
2026-07-28 15:51:42 -0300

    feat: adiciona PartidaResponseDto

---
+++ MatchTracker.Api/DTOs/Partidas/PartidaResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using MatchTracker.Api.Entities.Enums;
+
+namespace MatchTracker.Api.DTOs.Partidas
+{
+    public class PartidaResponseDto
+    {
+        public int Id { get; set; }
+        public MapaCs2 Mapa { get; set; }
+        public int Kills { get; set; }
+        public int Mortes { get; set; }
+        public int Assistencias { get; set; }
+        public ResultadoPartida Resultado { get; set; }
+        public DateTime DataPartida { get; set; }
+    }
+}
