5e9f8a4b5f49f22413f88dff7b64a23c7d01ee0e
Eduardo <eduardo.olvreis@gmail.com>
2026-07-30 02:32:10 -0300

    feat: adiciona inicio da PartidaService

---
+++ MatchTracker.Api/Services/PartidaService.cs
@@ -0,0 +1,13 @@
+﻿using MatchTracker.Api.Repositories;
+
+namespace MatchTracker.Api.Services
+{
+    public class PartidaService : IPartidaService
+    {
+        private readonly IPartidaRepository _repository;
+        public PartidaService(IPartidaRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
