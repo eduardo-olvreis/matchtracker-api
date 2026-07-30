0dfc5bc47b5af04691dc5f7694f1b944816dcf32
Eduardo <eduardo.olvreis@gmail.com>
2026-07-29 23:00:58 -0300

    feat: adiciona o escopo da partidaRepository na Program

--- MatchTracker.Api/Program.cs
+++ MatchTracker.Api/Program.cs
@@ -1,5 +1,7 @@
 using MatchTracker.Api.Data;
+using MatchTracker.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IPartidaRepository, SqlPartidaRepository>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
