fc9eb206640ff1636e14a80eb8701332a3bedd2a
Eduardo <eduardo.olvreis@gmail.com>
2026-07-29 23:44:34 -0300

    feat: adiciona migration do placar da partida

---
+++ MatchTracker.Api/Migrations/20260730024322_AddPlacarToPartida.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlacarToPartida : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlacarAdversario",
+                table: "Partidas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlacarTime",
+                table: "Partidas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlacarAdversario",
+                table: "Partidas");
+
+            migrationBuilder.DropColumn(
+                name: "PlacarTime",
+                table: "Partidas");
+        }
+    }
+}
