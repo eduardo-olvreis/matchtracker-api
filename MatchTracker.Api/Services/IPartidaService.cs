afa3fb613c6862e955b2606f9fe2fbee42abbe9f
Eduardo <eduardo.olvreis@gmail.com>
2026-07-30 02:28:35 -0300

    feat: adiciona Interface da Partida Service

---
+++ MatchTracker.Api/Services/IPartidaService.cs
@@ -0,0 +1,13 @@
+﻿using MatchTracker.Api.DTOs.Partidas;
+
+namespace MatchTracker.Api.Services
+{
+    public interface IPartidaService
+    {
+        public Task<IEnumerable<PartidaResponseDto>> GetAllAsync();
+        public Task<PartidaResponseDto?> GetByIdAsync(int id);
+        public Task<PartidaResponseDto> AddAsync(PartidaCreateDto partidaDto);
+        public Task<PartidaResponseDto> UpdateAsync(int id, PartidaCreateDto partidaDto);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
